f4a6fd4500a3efa5dfc1a24390da857f764a5cef
akshaaatt <tiwariakshat03@gmail.com>
2021-03-19 17:47:34 +0530

    Initial commit

---
+++ mlkit/src/main/java/com/limerse/mlkit/barcodedetection/BarcodeField.kt
@@ -0,0 +1,8 @@
+package com.limerse.mlkit.barcodedetection
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/** Information about a barcode field.  */
+@Parcelize
+data class BarcodeField(val label: String, val value: String) : Parcelable
---
+++ mlkit/src/main/java/com/limerse/mlkit/productsearch/Product.kt
@@ -0,0 +1,4 @@
+package com.limerse.mlkit.productsearch
+
+/** Information about a product.  */
+data class Product(val imageUrl: String, val title: String, val subtitle: String)
